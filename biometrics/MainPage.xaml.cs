2945ddfd8f0bfd4d549928d472fb75978a125cce
James Westgate <jameswestgate@icloud.com>
2019-10-08 17:55:31 +0100

    Basic biometrics working,

--- biometrics/MainPage.xaml.cs
+++ biometrics/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
 using Xamarin.Forms;
 
 namespace biometrics
@@ -16,6 +18,51 @@ namespace biometrics
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //Lets first try do some fingerprint shit here yo
+            Task.Run(async () => await StartAuthenticationProcess());
+        }
+
+        private async Task StartAuthenticationProcess()
+        {
+            //Lets first try do some fingerprint shit here yo
+
+            // Code to run on the main thread
+            try
+            {
+                Console.WriteLine("STARTING ALL THE AUTHENTICATION NOW.");
+
+                var result = await AuthenticateWithBiometrics();
+
+                Console.WriteLine("MAKING TASK GO.");
+
+                //task.GetAwaiter().GetResult();
+
+                if (result.Authenticated)
+                {
+                    Console.WriteLine("ALL THE COOKIES BELONG TO YOU.");
+                }
+                else
+                {
+                    Console.WriteLine("NOT ALLOWED THE SECRETS.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"SAD PANDA ERROR. {ex}");
+            }
+        }
+
+        private async Task<FingerprintAuthenticationResult> AuthenticateWithBiometrics()
+        {
+            var config = new AuthenticationRequestConfiguration("Need your body to unlock the secrets!");
+
+            return await CrossFingerprint.Current.AuthenticateAsync(config);
         }
     }
 }
